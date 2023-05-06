23ff4aa5effc4cc9e3abd4cc209a679f6b212bf5
Juan Fran <86612578+juanfrancisc@users.noreply.github.com>
2023-05-06 22:19:59 +0200

    Contador de canastas

---
+++ sumar.js
@@ -0,0 +1,32 @@
+let numero = 0;
+/*boton.onclick = () => {
+ numero++;
+ mostrador.value = numero;
+}*/
+
+/*onkeypress = () => {
+    numero++;
+    mostrador.value = numero;
+   }*/
+
+window.onload = function() {
+    document.onkeyup = muestraInformacion;
+    /*document.onkeypress = muestraInformacion;
+    /*document.onkeydown = muestraInformacion;*/
+  }
+  
+  function muestraInformacion(elEvento) {
+    var evento = window.event || elEvento;
+    console.log(evento.keyCode)
+    if (evento.keyCode === 33){
+        numero++;
+        mostrador.value = numero;
+        alimentos.value = numero;
+    } else {
+        numero--;
+        mostrador.value = numero;
+        alimentos.value = numero;
+    }
+
+        
+  }
